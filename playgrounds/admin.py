a75cfa38a1137b72aa0302b7c8ff90b0b2053a63
pablo727 <pablo.rodrguezsanz@yahoo.com>
2025-07-31 21:25:53 +0200

    Add playground admin for PromptTestRun

--- playgrounds/admin.py
+++ playgrounds/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PromptTestRun
+
+
+class PromptTestRunAdmin(admin.ModelAdmin):
+    model = PromptTestRun
+    list_display = (
+        "user",
+        "input_variables",
+        "llm_used",
+        "result",
+        "tokens_used",
+        "latency_ms",
+        "created_at",
+    )
+    search_fields = ("result", "user__username")
+    list_filter = ("input_variables", "llm_used", "created_at")
+    readonly_fields = ("llm_used", "tokens_used", "latency_ms", "created_at")
+    ordering = ("-created_at", "llm_used")
+
+
+admin.site.register(PromptTestRun)
